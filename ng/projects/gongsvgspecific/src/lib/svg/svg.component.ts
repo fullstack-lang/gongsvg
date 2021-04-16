c1a92c907ee6eb3edc237a57430061f3ab99db72
peugeot <thomaspeugeot@yahoo.fr>
2021-04-16 08:28:32 +0200

    First display

--- ng/projects/gongsvgspecific/src/lib/svg/svg.component.ts
+++ ng/projects/gongsvgspecific/src/lib/svg/svg.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable, Subscription, timer } from 'rxjs';
+
+import * as gongsvg from 'gongsvg'
 
 @Component({
   selector: 'lib-svg',
-  templateUrl: './svg.component.html',
+  templateUrl: './svg.component.svg',
   styleUrls: ['./svg.component.css']
 })
 export class SvgComponent implements OnInit {
 
-  constructor() { }
+  SVG: string
+
+  rect: number
+
+  /**
+ * the component is refreshed when modification are performed in the back repo 
+ * 
+ * the checkCommitNbTimer polls the commit number of the back repo
+ * if the commit number has increased, it pulls the front repo and redraw the diagram
+ */
+  checkCommitNbTimer: Observable<number> = timer(500, 500);
+  lastCommitNb = -1
+  currTime: number
+
+  constructor(
+    private gongsvgFrontRepoService: gongsvg.FrontRepoService,
+    private gongsvgCommitNbService: gongsvg.CommitNbService,
+  ) { }
 
   ngOnInit(): void {
+
+    this.checkCommitNbTimer.subscribe(
+      currTime => {
+        this.currTime = currTime
+
+        // see above for the explanation
+        this.gongsvgCommitNbService.getCommitNb().subscribe(
+          commitNb => {
+            if (this.lastCommitNb < commitNb) {
+
+              console.log("last commit nb " + this.lastCommitNb + " new: " + commitNb)
+              this.refresh()
+              this.lastCommitNb = commitNb
+            }
+          }
+        )
+      }
+    )
+  }
+
+  refresh(): void {
+  }
+
+  fillColor = 'rgb(255, 0, 0)';
+  Text = "Toto"
+  width = 150
+  heigth = 200
+  changeColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    this.fillColor = `rgb(${r}, ${g}, ${b})`;
   }
 
 }

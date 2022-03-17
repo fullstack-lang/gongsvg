7202607c1a59801471048781854ea2f48f8a40b9
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-03-17 17:25:02 +0100

    added ColorEnum

--- go/models/gong.go
+++ go/models/gong.go
@@ -2398,3 +2398,610 @@ func generatesIdentifier(gongStructName string, idx int, instanceName string) (i
 }
 
 // insertion point of enum utility functions
+// Utility function for ColorType
+// if enum values are string, it is stored with the value
+// if enum values are int, they are stored with the code of the value
+func (colortype ColorType) ToString() (res string) {
+
+	// migration of former implementation of enum
+	switch colortype {
+	// insertion code per enum code
+	case Aliceblue:
+		res = "aliceblue"
+	case Antiquewhite:
+		res = "antiquewhite"
+	case Aqua:
+		res = "aqua"
+	case Aquamarine:
+		res = "aquamarine "
+	case Azure:
+		res = "azure"
+	case Beige:
+		res = "beige"
+	case Bisque:
+		res = "bisque "
+	case Black:
+		res = "black"
+	case Blanchedalmond:
+		res = "blanchedalmond "
+	case Blue:
+		res = "blue"
+	case Blueviolet:
+		res = "blueviolet "
+	case Brown:
+		res = "brown"
+	case Burlywood:
+		res = "burlywood"
+	case Cadetblue:
+		res = "cadetblue"
+	case Chartreuse:
+		res = "chartreuse "
+	case Chocolate:
+		res = "chocolate"
+	case Coral:
+		res = "coral"
+	case Cornflowerblue:
+		res = "cornflowerblue "
+	case Cornsilk:
+		res = "cornsilk"
+	case Crimson:
+		res = "crimson"
+	case Cyan:
+		res = "cyan"
+	case Darkblue:
+		res = "darkblue"
+	case Darkcyan:
+		res = "darkcyan"
+	case Darkgoldenrod:
+		res = "darkgoldenrod"
+	case Darkgray:
+		res = "darkgray"
+	case Darkgreen:
+		res = "darkgreen"
+	case Darkgrey:
+		res = "darkgrey"
+	case Darkkhaki:
+		res = "darkkhaki"
+	case Darkmagenta:
+		res = "darkmagenta"
+	case Darkolivegreen:
+		res = "darkolivegreen "
+	case Darkorange:
+		res = "darkorange "
+	case Darkorchid:
+		res = "darkorchid "
+	case Darkred:
+		res = "darkred"
+	case Darksalmon:
+		res = "darksalmon "
+	case Darkseagreen:
+		res = "darkseagreen"
+	case Darkslateblue:
+		res = "darkslateblue"
+	case Darkslategray:
+		res = "darkslategray"
+	case Darkslategrey:
+		res = "darkslategrey"
+	case Darkturquoise:
+		res = "darkturquoise"
+	case Darkviolet:
+		res = "darkviolet "
+	case Deeppink:
+		res = "deeppink"
+	case Deepskyblue:
+		res = "deepskyblue"
+	case Dimgray:
+		res = "dimgray"
+	case Dimgrey:
+		res = "dimgrey"
+	case Dodgerblue:
+		res = "dodgerblue "
+	case Firebrick:
+		res = "firebrick"
+	case Floralwhite:
+		res = "floralwhite"
+	case Forestgreen:
+		res = "forestgreen"
+	case Fuchsia:
+		res = "fuchsia"
+	case Gainsboro:
+		res = "gainsboro"
+	case Ghostwhite:
+		res = "ghostwhite "
+	case Gold:
+		res = "gold"
+	case Goldenrod:
+		res = "goldenrod"
+	case Gray:
+		res = "gray"
+	case Green:
+		res = "green"
+	case Greenyellow:
+		res = "greenyellow"
+	case Grey:
+		res = "grey"
+	case Honeydew:
+		res = "honeydew"
+	case Hotpink:
+		res = "hotpink"
+	case Indianred:
+		res = "indianred"
+	case Indigo:
+		res = "indigo "
+	case Ivory:
+		res = "ivory"
+	case Khaki:
+		res = "khaki"
+	case Lavender:
+		res = "lavender"
+	case Lavenderblush:
+		res = "lavenderblush"
+	case Lawngreen:
+		res = "lawngreen"
+	case Lemonchiffon:
+		res = "lemonchiffon"
+	case Lightblue:
+		res = "lightblue"
+	case Lightcoral:
+		res = "lightcoral "
+	case Lightcyan:
+		res = "lightcyan"
+	case Lightgoldenrodyellow:
+		res = "lightgoldenrodyellow"
+	case Lightgray:
+		res = "lightgray"
+	case Lightgreen:
+		res = "lightgreen "
+	case Lightgrey:
+		res = "lightgrey"
+	case Lightpink:
+		res = "lightpink"
+	case Lightsalmon:
+		res = "lightsalmon"
+	case Lightseagreen:
+		res = "lightseagreen"
+	case Lightskyblue:
+		res = "lightskyblue"
+	case Lightslategray:
+		res = "lightslategray "
+	case Lightslategrey:
+		res = "lightslategrey "
+	case Lightsteelblue:
+		res = "lightsteelblue "
+	case Lightyellow:
+		res = "lightyellow"
+	case Lime:
+		res = "lime"
+	case Limegreen:
+		res = "limegreen"
+	case Linen:
+		res = "linen"
+	case Magenta:
+		res = "magenta"
+	case Maroon:
+		res = "maroon "
+	case Mediumaquamarine:
+		res = "mediumaquamarine"
+	case Mediumblue:
+		res = "mediumblue "
+	case Mediumorchid:
+		res = "mediumorchid"
+	case Mediumpurple:
+		res = "mediumpurple"
+	case Mediumseagreen:
+		res = "mediumseagreen "
+	case Mediumslateblue:
+		res = "mediumslateblue"
+	case Mediumspringgreen:
+		res = "mediumspringgreen"
+	case Mediumturquoise:
+		res = "mediumturquoise"
+	case Mediumvioletred:
+		res = "mediumvioletred"
+	case Midnightblue:
+		res = "midnightblue"
+	case Mintcream:
+		res = "mintcream"
+	case Mistyrose:
+		res = "mistyrose"
+	case Moccasin:
+		res = "moccasin"
+	case Navajowhite:
+		res = "navajowhite"
+	case Navy:
+		res = "navy"
+	case Oldlace:
+		res = "oldlace"
+	case Olive:
+		res = "olive"
+	case Olivedrab:
+		res = "olivedrab"
+	case Orange:
+		res = "orange "
+	case Orangered:
+		res = "orangered"
+	case Orchid:
+		res = "orchid "
+	case Palegoldenrod:
+		res = "palegoldenrod"
+	case Palegreen:
+		res = "palegreen"
+	case Paleturquoise:
+		res = "paleturquoise"
+	case Palevioletred:
+		res = "palevioletred"
+	case Papayawhip:
+		res = "papayawhip "
+	case Peachpuff:
+		res = "peachpuff"
+	case Peru:
+		res = "peru"
+	case Pink:
+		res = "pink"
+	case Plum:
+		res = "plum"
+	case Powderblue:
+		res = "powderblue "
+	case Purple:
+		res = "purple "
+	case Red:
+		res = "red"
+	case Rosybrown:
+		res = "rosybrown"
+	case Royalblue:
+		res = "royalblue"
+	case Saddlebrown:
+		res = "saddlebrown"
+	case Salmon:
+		res = "salmon "
+	case Sandybrown:
+		res = "sandybrown "
+	case Seagreen:
+		res = "seagreen"
+	case Seashell:
+		res = "seashell"
+	case Sienna:
+		res = "sienna "
+	case Silver:
+		res = "silver "
+	case Skyblue:
+		res = "skyblue"
+	case Slateblue:
+		res = "slateblue"
+	case Slategray:
+		res = "slategray"
+	case Slategrey:
+		res = "slategrey"
+	case Snow:
+		res = "snow"
+	case Springgreen:
+		res = "springgreen"
+	case Steelblue:
+		res = "steelblue"
+	case Tan:
+		res = "tan"
+	case Teal:
+		res = "teal"
+	case Thistle:
+		res = "thistle"
+	case Tomato:
+		res = "tomato "
+	case Turquoise:
+		res = "turquoise"
+	case Violet:
+		res = "violet "
+	case Wheat:
+		res = "wheat"
+	case White:
+		res = "white"
+	case Whitesmoke:
+		res = "whitesmoke "
+	case Yellow:
+		res = "yellow "
+	case Yellowgreen:
+		res = "yellowgreen"
+	}
+	return
+}
+
+func (colortype *ColorType) FromString(input string) {
+
+	switch input {
+	// insertion code per enum code
+	case "aliceblue":
+		*colortype = Aliceblue
+	case "antiquewhite":
+		*colortype = Antiquewhite
+	case "aqua":
+		*colortype = Aqua
+	case "aquamarine ":
+		*colortype = Aquamarine
+	case "azure":
+		*colortype = Azure
+	case "beige":
+		*colortype = Beige
+	case "bisque ":
+		*colortype = Bisque
+	case "black":
+		*colortype = Black
+	case "blanchedalmond ":
+		*colortype = Blanchedalmond
+	case "blue":
+		*colortype = Blue
+	case "blueviolet ":
+		*colortype = Blueviolet
+	case "brown":
+		*colortype = Brown
+	case "burlywood":
+		*colortype = Burlywood
+	case "cadetblue":
+		*colortype = Cadetblue
+	case "chartreuse ":
+		*colortype = Chartreuse
+	case "chocolate":
+		*colortype = Chocolate
+	case "coral":
+		*colortype = Coral
+	case "cornflowerblue ":
+		*colortype = Cornflowerblue
+	case "cornsilk":
+		*colortype = Cornsilk
+	case "crimson":
+		*colortype = Crimson
+	case "cyan":
+		*colortype = Cyan
+	case "darkblue":
+		*colortype = Darkblue
+	case "darkcyan":
+		*colortype = Darkcyan
+	case "darkgoldenrod":
+		*colortype = Darkgoldenrod
+	case "darkgray":
+		*colortype = Darkgray
+	case "darkgreen":
+		*colortype = Darkgreen
+	case "darkgrey":
+		*colortype = Darkgrey
+	case "darkkhaki":
+		*colortype = Darkkhaki
+	case "darkmagenta":
+		*colortype = Darkmagenta
+	case "darkolivegreen ":
+		*colortype = Darkolivegreen
+	case "darkorange ":
+		*colortype = Darkorange
+	case "darkorchid ":
+		*colortype = Darkorchid
+	case "darkred":
+		*colortype = Darkred
+	case "darksalmon ":
+		*colortype = Darksalmon
+	case "darkseagreen":
+		*colortype = Darkseagreen
+	case "darkslateblue":
+		*colortype = Darkslateblue
+	case "darkslategray":
+		*colortype = Darkslategray
+	case "darkslategrey":
+		*colortype = Darkslategrey
+	case "darkturquoise":
+		*colortype = Darkturquoise
+	case "darkviolet ":
+		*colortype = Darkviolet
+	case "deeppink":
+		*colortype = Deeppink
+	case "deepskyblue":
+		*colortype = Deepskyblue
+	case "dimgray":
+		*colortype = Dimgray
+	case "dimgrey":
+		*colortype = Dimgrey
+	case "dodgerblue ":
+		*colortype = Dodgerblue
+	case "firebrick":
+		*colortype = Firebrick
+	case "floralwhite":
+		*colortype = Floralwhite
+	case "forestgreen":
+		*colortype = Forestgreen
+	case "fuchsia":
+		*colortype = Fuchsia
+	case "gainsboro":
+		*colortype = Gainsboro
+	case "ghostwhite ":
+		*colortype = Ghostwhite
+	case "gold":
+		*colortype = Gold
+	case "goldenrod":
+		*colortype = Goldenrod
+	case "gray":
+		*colortype = Gray
+	case "green":
+		*colortype = Green
+	case "greenyellow":
+		*colortype = Greenyellow
+	case "grey":
+		*colortype = Grey
+	case "honeydew":
+		*colortype = Honeydew
+	case "hotpink":
+		*colortype = Hotpink
+	case "indianred":
+		*colortype = Indianred
+	case "indigo ":
+		*colortype = Indigo
+	case "ivory":
+		*colortype = Ivory
+	case "khaki":
+		*colortype = Khaki
+	case "lavender":
+		*colortype = Lavender
+	case "lavenderblush":
+		*colortype = Lavenderblush
+	case "lawngreen":
+		*colortype = Lawngreen
+	case "lemonchiffon":
+		*colortype = Lemonchiffon
+	case "lightblue":
+		*colortype = Lightblue
+	case "lightcoral ":
+		*colortype = Lightcoral
+	case "lightcyan":
+		*colortype = Lightcyan
+	case "lightgoldenrodyellow":
+		*colortype = Lightgoldenrodyellow
+	case "lightgray":
+		*colortype = Lightgray
+	case "lightgreen ":
+		*colortype = Lightgreen
+	case "lightgrey":
+		*colortype = Lightgrey
+	case "lightpink":
+		*colortype = Lightpink
+	case "lightsalmon":
+		*colortype = Lightsalmon
+	case "lightseagreen":
+		*colortype = Lightseagreen
+	case "lightskyblue":
+		*colortype = Lightskyblue
+	case "lightslategray ":
+		*colortype = Lightslategray
+	case "lightslategrey ":
+		*colortype = Lightslategrey
+	case "lightsteelblue ":
+		*colortype = Lightsteelblue
+	case "lightyellow":
+		*colortype = Lightyellow
+	case "lime":
+		*colortype = Lime
+	case "limegreen":
+		*colortype = Limegreen
+	case "linen":
+		*colortype = Linen
+	case "magenta":
+		*colortype = Magenta
+	case "maroon ":
+		*colortype = Maroon
+	case "mediumaquamarine":
+		*colortype = Mediumaquamarine
+	case "mediumblue ":
+		*colortype = Mediumblue
+	case "mediumorchid":
+		*colortype = Mediumorchid
+	case "mediumpurple":
+		*colortype = Mediumpurple
+	case "mediumseagreen ":
+		*colortype = Mediumseagreen
+	case "mediumslateblue":
+		*colortype = Mediumslateblue
+	case "mediumspringgreen":
+		*colortype = Mediumspringgreen
+	case "mediumturquoise":
+		*colortype = Mediumturquoise
+	case "mediumvioletred":
+		*colortype = Mediumvioletred
+	case "midnightblue":
+		*colortype = Midnightblue
+	case "mintcream":
+		*colortype = Mintcream
+	case "mistyrose":
+		*colortype = Mistyrose
+	case "moccasin":
+		*colortype = Moccasin
+	case "navajowhite":
+		*colortype = Navajowhite
+	case "navy":
+		*colortype = Navy
+	case "oldlace":
+		*colortype = Oldlace
+	case "olive":
+		*colortype = Olive
+	case "olivedrab":
+		*colortype = Olivedrab
+	case "orange ":
+		*colortype = Orange
+	case "orangered":
+		*colortype = Orangered
+	case "orchid ":
+		*colortype = Orchid
+	case "palegoldenrod":
+		*colortype = Palegoldenrod
+	case "palegreen":
+		*colortype = Palegreen
+	case "paleturquoise":
+		*colortype = Paleturquoise
+	case "palevioletred":
+		*colortype = Palevioletred
+	case "papayawhip ":
+		*colortype = Papayawhip
+	case "peachpuff":
+		*colortype = Peachpuff
+	case "peru":
+		*colortype = Peru
+	case "pink":
+		*colortype = Pink
+	case "plum":
+		*colortype = Plum
+	case "powderblue ":
+		*colortype = Powderblue
+	case "purple ":
+		*colortype = Purple
+	case "red":
+		*colortype = Red
+	case "rosybrown":
+		*colortype = Rosybrown
+	case "royalblue":
+		*colortype = Royalblue
+	case "saddlebrown":
+		*colortype = Saddlebrown
+	case "salmon ":
+		*colortype = Salmon
+	case "sandybrown ":
+		*colortype = Sandybrown
+	case "seagreen":
+		*colortype = Seagreen
+	case "seashell":
+		*colortype = Seashell
+	case "sienna ":
+		*colortype = Sienna
+	case "silver ":
+		*colortype = Silver
+	case "skyblue":
+		*colortype = Skyblue
+	case "slateblue":
+		*colortype = Slateblue
+	case "slategray":
+		*colortype = Slategray
+	case "slategrey":
+		*colortype = Slategrey
+	case "snow":
+		*colortype = Snow
+	case "springgreen":
+		*colortype = Springgreen
+	case "steelblue":
+		*colortype = Steelblue
+	case "tan":
+		*colortype = Tan
+	case "teal":
+		*colortype = Teal
+	case "thistle":
+		*colortype = Thistle
+	case "tomato ":
+		*colortype = Tomato
+	case "turquoise":
+		*colortype = Turquoise
+	case "violet ":
+		*colortype = Violet
+	case "wheat":
+		*colortype = Wheat
+	case "white":
+		*colortype = White
+	case "whitesmoke ":
+		*colortype = Whitesmoke
+	case "yellow ":
+		*colortype = Yellow
+	case "yellowgreen":
+		*colortype = Yellowgreen
+	}
+}
+
--- ng/projects/gongsvg/src/public-api.ts
+++ ng/projects/gongsvg/src/public-api.ts
@@ -85,3 +85,4 @@ export * from './lib/text-db'
 export * from './lib/text.service'
 
 
+export * from './lib/ColorType'

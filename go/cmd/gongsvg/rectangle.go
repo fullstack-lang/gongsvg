3424a94ec1a60c74b362651707c13fec4d78393b
peugeot <thomaspeugeot@yahoo.fr>
2023-05-15 05:31:06 +0200

    Link : not robust to pageX not zero Fixes #81

--- go/cmd/gongsvg/rectangle.go
+++ go/cmd/gongsvg/rectangle.go
@@ -123,8 +123,8 @@ Line 2`
 	__AnchoredText__000001_Start_Anchored_1.Content = `Start Anchored 1
 Second line
 Third Line`
-	__AnchoredText__000001_Start_Anchored_1.X_Offset = 13.000000
-	__AnchoredText__000001_Start_Anchored_1.Y_Offset = -49.000000
+	__AnchoredText__000001_Start_Anchored_1.X_Offset = 37.000000
+	__AnchoredText__000001_Start_Anchored_1.Y_Offset = -121.000000
 	__AnchoredText__000001_Start_Anchored_1.FontWeight = `light`
 	__AnchoredText__000001_Start_Anchored_1.Color = `cyan`
 	__AnchoredText__000001_Start_Anchored_1.FillOpacity = 100.000000
@@ -189,10 +189,10 @@ Third Line`
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.StartAnchorType = models.ANCHOR_CENTER
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.EndAnchorType = models.ANCHOR_CENTER
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.StartOrientation = models.ORIENTATION_HORIZONTAL
-	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.StartRatio = 0.261682
+	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.StartRatio = 0.481308
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.EndOrientation = models.ORIENTATION_HORIZONTAL
-	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.EndRatio = 0.741935
-	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.CornerOffsetRatio = 1.750973
+	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.EndRatio = 0.548387
+	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.CornerOffsetRatio = -1.074000
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.CornerRadius = 8.000000
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.HasEndArrow = true
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.EndArrowSize = 10.000000
@@ -248,8 +248,8 @@ Third Line`
 
 	// Rect values setup
 	__Rect__000000_Bottom.Name = `Bottom`
-	__Rect__000000_Bottom.X = 358.000000
-	__Rect__000000_Bottom.Y = 496.000000
+	__Rect__000000_Bottom.X = 475.000000
+	__Rect__000000_Bottom.Y = 447.000000
 	__Rect__000000_Bottom.Width = 640.000000
 	__Rect__000000_Bottom.Height = 248.000000
 	__Rect__000000_Bottom.RX = 5.000000
@@ -275,8 +275,8 @@ Third Line`
 
 	// Rect values setup
 	__Rect__000001_Middle_Rect.Name = `Middle Rect`
-	__Rect__000001_Middle_Rect.X = 393.000000
-	__Rect__000001_Middle_Rect.Y = 183.000000
+	__Rect__000001_Middle_Rect.X = 235.000000
+	__Rect__000001_Middle_Rect.Y = 190.000000
 	__Rect__000001_Middle_Rect.Width = 200.000000
 	__Rect__000001_Middle_Rect.Height = 150.000000
 	__Rect__000001_Middle_Rect.RX = 3.000000
@@ -302,9 +302,9 @@ Third Line`
 
 	// Rect values setup
 	__Rect__000002_Top.Name = `Top`
-	__Rect__000002_Top.X = 945.000000
-	__Rect__000002_Top.Y = 103.000000
-	__Rect__000002_Top.Width = 257.000000
+	__Rect__000002_Top.X = 681.000000
+	__Rect__000002_Top.Y = 128.000000
+	__Rect__000002_Top.Width = 500.000000
 	__Rect__000002_Top.Height = 214.000000
 	__Rect__000002_Top.RX = 3.000000
 	__Rect__000002_Top.Color = `lightcyan`

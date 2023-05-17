c15cbefc2dc79799dfde12522f854a3928f48f49
peugeot <thomaspeugeot@yahoo.fr>
2023-05-17 05:45:28 +0200

    Page problem with links #86

--- go/cmd/gongsvg/rectangle.go
+++ go/cmd/gongsvg/rectangle.go
@@ -117,8 +117,8 @@ Line 2`
 	__AnchoredText__000001_Start_Anchored_1.Content = `Start Anchored 1
 Second line
 Third Line`
-	__AnchoredText__000001_Start_Anchored_1.X_Offset = -128.000000
-	__AnchoredText__000001_Start_Anchored_1.Y_Offset = -85.987488
+	__AnchoredText__000001_Start_Anchored_1.X_Offset = -272.000000
+	__AnchoredText__000001_Start_Anchored_1.Y_Offset = -65.987488
 	__AnchoredText__000001_Start_Anchored_1.FontWeight = `light`
 	__AnchoredText__000001_Start_Anchored_1.Color = `cyan`
 	__AnchoredText__000001_Start_Anchored_1.FillOpacity = 100.000000
@@ -166,11 +166,11 @@ Third Line`
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.Type = models.LINK_TYPE_FLOATING_ORTHOGONAL
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.StartAnchorType = models.ANCHOR_CENTER
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.EndAnchorType = models.ANCHOR_CENTER
-	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.StartOrientation = models.ORIENTATION_HORIZONTAL
-	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.StartRatio = 0.000000
+	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.StartOrientation = models.ORIENTATION_VERTICAL
+	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.StartRatio = 0.105546
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.EndOrientation = models.ORIENTATION_VERTICAL
-	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.EndRatio = 0.129139
-	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.CornerOffsetRatio = -0.089445
+	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.EndRatio = 0.182119
+	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.CornerOffsetRatio = 1.308411
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.CornerRadius = 8.000000
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.HasEndArrow = true
 	__Link__000000_Arrow_Top_to_Bottom_vertical_to_horizontal.EndArrowSize = 10.000000
@@ -184,10 +184,10 @@ Third Line`
 
 	// Rect values setup
 	__Rect__000000_Bottom.Name = `Bottom`
-	__Rect__000000_Bottom.X = 253.000000
-	__Rect__000000_Bottom.Y = 580.000000
+	__Rect__000000_Bottom.X = 36.000000
+	__Rect__000000_Bottom.Y = 508.000000
 	__Rect__000000_Bottom.Width = 604.000000
-	__Rect__000000_Bottom.Height = 38.000000
+	__Rect__000000_Bottom.Height = 253.000000
 	__Rect__000000_Bottom.RX = 5.000000
 	__Rect__000000_Bottom.Color = `bisque`
 	__Rect__000000_Bottom.FillOpacity = 50.000000
@@ -238,8 +238,8 @@ Third Line`
 
 	// Rect values setup
 	__Rect__000002_Top.Name = `Top`
-	__Rect__000002_Top.X = 205.000000
-	__Rect__000002_Top.Y = 83.000000
+	__Rect__000002_Top.X = 203.000000
+	__Rect__000002_Top.Y = 187.000000
 	__Rect__000002_Top.Width = 1118.000000
 	__Rect__000002_Top.Height = 214.000000
 	__Rect__000002_Top.RX = 3.000000

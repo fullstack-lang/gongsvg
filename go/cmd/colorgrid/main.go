d42df902713e8e508e32d882542ed4cf553ee8d2
peugeot <thomaspeugeot@yahoo.fr>
2023-09-09 03:06:44 +0200

    new gongc

--- go/cmd/colorgrid/main.go
+++ go/cmd/colorgrid/main.go
@@ -31,7 +31,7 @@ func main() {
 
 	// setup GORM
 	r := gongsvg_static.ServeStaticFiles(false)
-	stage := gongsvg_fullstack.NewStackInstance(r, "github.com/fullstack-lang/gongsvg/go/models")
+	stage, _ := gongsvg_fullstack.NewStackInstance(r, "github.com/fullstack-lang/gongsvg/go/models")
 
 	colorLayer := (&gongsvg_models.Layer{Name: "color"}).Stage(stage)
 	colorLayer.Display = true
--- go/cmd/gongsvg/main.go
+++ go/cmd/gongsvg/main.go
@@ -4,31 +4,29 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
-	"strings"
+	"strconv"
 
 	gongsvg_go "github.com/fullstack-lang/gongsvg/go"
+	gongsvg_data "github.com/fullstack-lang/gongsvg/go/data"
 	gongsvg_fullstack "github.com/fullstack-lang/gongsvg/go/fullstack"
 	gongsvg_models "github.com/fullstack-lang/gongsvg/go/models"
+	gongsvg_orm "github.com/fullstack-lang/gongsvg/go/orm"
 	gongsvg_static "github.com/fullstack-lang/gongsvg/go/static"
 
 	gongdoc_load "github.com/fullstack-lang/gongdoc/go/load"
 )
 
 var (
-	logDBFlag  = flag.Bool("logDB", false, "log mode for db")
 	logGINFlag = flag.Bool("logGIN", false, "log mode for gin")
 
-	marshallOnStartup  = flag.String("marshallOnStartup", "", "at startup, marshall staged data to a go file with the marshall name and '.go' (must be lowercased without spaces). If marshall arg is '', no marshalling")
 	unmarshallFromCode = flag.String("unmarshallFromCode", "", "unmarshall data from go file and '.go' (must be lowercased without spaces), If unmarshallFromCode arg is '', no unmarshalling")
-	unmarshall         = flag.String("unmarshall", "", "unmarshall data from marshall name and '.go' (must be lowercased without spaces), If unmarshall arg is '', no unmarshalling")
 	marshallOnCommit   = flag.String("marshallOnCommit", "", "on all commits, marshall staged data to a go file with the marshall name and '.go' (must be lowercased without spaces). If marshall arg is '', no marshalling")
 
 	diagrams         = flag.Bool("diagrams", true, "parse/analysis go/models and go/diagrams")
 	embeddedDiagrams = flag.Bool("embeddedDiagrams", false, "parse/analysis go/models and go/embeddedDiagrams")
 
-	benchmark = flag.Int("benchmark", -1, "benchmark")
+	port = flag.Int("port", 8080, "port server")
 )
 
 // InjectionGateway is the singloton that stores all functions
@@ -64,48 +62,17 @@ func main() {
 
 	// setup stack
 	var stage *gongsvg_models.StageStruct
+	var backRepo *gongsvg_orm.BackRepoStruct
+
 	if *marshallOnCommit != "" {
 		// persistence in a SQLite file on disk in memory
-		stage = gongsvg_fullstack.NewStackInstance(r, "gongsvg")
+		stage, backRepo = gongsvg_fullstack.NewStackInstance(r, "gongsvg")
 	} else {
 		// persistence in a SQLite file on disk
-		stage = gongsvg_fullstack.NewStackInstance(r, "gongsvg", "./test.db")
+		stage, backRepo = gongsvg_fullstack.NewStackInstance(r, "gongsvg", "./gongsvg.db")
 	}
 
-	// generate injection code from the stage
-	if *marshallOnStartup != "" {
-
-		if strings.Contains(*marshallOnStartup, " ") {
-			log.Fatalln(*marshallOnStartup + " must not contains blank spaces")
-		}
-		if strings.ToLower(*marshallOnStartup) != *marshallOnStartup {
-			log.Fatalln(*marshallOnStartup + " must be lowercases")
-		}
-
-		file, err := os.Create(fmt.Sprintf("./%s.go", *marshallOnStartup))
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		defer file.Close()
-
-		stage.Checkout()
-		stage.Marshall(file, "github.com/fullstack-lang/gongsvg/go/models", "main")
-		os.Exit(0)
-	}
-
-	// setup the stage by injecting the code from code database
-	if *unmarshall != "" {
-		stage.Checkout()
-		stage.Reset()
-		stage.Commit()
-		if InjectionGateway[*unmarshall] != nil {
-			InjectionGateway[*unmarshall]()
-		}
-		stage.Commit()
-	} else {
-		// in case the database is used, checkout the content to the stage
-		stage.Checkout()
-	}
+	gongsvg_data.Load(r, gongsvg_go.GoModelsDir, "gongsvg", stage, backRepo)
 
 	if *unmarshallFromCode != "" {
 		stage.Checkout()
@@ -128,177 +95,7 @@ func main() {
 	// hook automatic marshall to go code at every commit
 	if *marshallOnCommit != "" {
 		hook := new(BeforeCommitImplementation)
-		stage.OnInitCommitFromFrontCallback = hook
-	}
-
-	if *benchmark != -1 {
-		heightBetweenRect := 50.0
-
-		stage.Reset()
-		stage.Commit()
-
-		svg := new(gongsvg_models.SVG).Stage(stage)
-		svg.Name = "Bench"
-		svg.IsEditable = true
-
-		for i := 0; i < *benchmark; i++ {
-			layer := new(gongsvg_models.Layer).Stage(stage)
-			svg.Layers = append(svg.Layers, layer)
-			layer.Name = fmt.Sprintf("%d", i)
-
-			startRect := (&gongsvg_models.Rect{Name: "Start " + layer.Name}).Stage(stage)
-			layer.Rects = append(layer.Rects, startRect)
-			startRect.X = 50
-			startRect.Y = float64(i+1) * heightBetweenRect
-
-			startRect.Width = 50
-			startRect.Height = 50 / 1.618
-
-			startRect.Stroke = gongsvg_models.Lightsalmon.ToString()
-			startRect.StrokeWidth = 1
-			startRect.StrokeDashArrayWhenSelected = "5 5"
-
-			startRect.FillOpacity = 100
-			startRect.Color = gongsvg_models.Lightsalmon.ToString()
-
-			// moveability
-			startRect.CanMoveHorizontaly = true
-			startRect.CanMoveVerticaly = true
-
-			// resizing
-			startRect.IsSelectable = true
-			startRect.CanHaveBottomHandle = true
-			startRect.CanHaveLeftHandle = true
-			startRect.CanHaveTopHandle = true
-			startRect.CanHaveRightHandle = true
-
-			title := new(gongsvg_models.RectAnchoredText).Stage(stage)
-			title.Name = fmt.Sprintf("%d", i)
-			title.Content = title.Name
-			title.X_Offset = 0
-			title.Y_Offset = 20
-			title.RectAnchorType = gongsvg_models.RECT_ANCHOR_TOP
-			title.TextAnchorType = gongsvg_models.TEXT_ANCHOR_CENTER
-			title.FontWeight = "bold"
-			title.Color = gongsvg_models.Black.ToString()
-			title.FillOpacity = 1.0
-			startRect.RectAnchoredTexts = append(startRect.RectAnchoredTexts, title)
-
-			endRect := (&gongsvg_models.Rect{Name: "End " + layer.Name}).Stage(stage)
-			layer.Rects = append(layer.Rects, endRect)
-			endRect.X = 50 + 400
-			endRect.Y = float64(i+1) * heightBetweenRect
-
-			endRect.Width = 50
-			endRect.Height = 50 / 1.618
-
-			endRect.Stroke = gongsvg_models.Lightsalmon.ToString()
-			endRect.StrokeWidth = 1
-			endRect.StrokeDashArrayWhenSelected = "5 5"
-
-			endRect.FillOpacity = 100
-			endRect.Color = gongsvg_models.Lightsalmon.ToString()
-
-			// moveability
-			endRect.CanMoveHorizontaly = true
-			endRect.CanMoveVerticaly = true
-
-			// resizing
-			endRect.IsSelectable = true
-			endRect.CanHaveBottomHandle = true
-			endRect.CanHaveLeftHandle = true
-			endRect.CanHaveTopHandle = true
-			endRect.CanHaveRightHandle = true
-
-			link := new(gongsvg_models.Link).Stage(stage)
-			link.Name = startRect.Name + " - to - " + endRect.Name
-
-			linkLayer := new(gongsvg_models.Layer).Stage(stage)
-
-			linkLayer.Links = append(linkLayer.Links, link)
-			svg.Layers = append(svg.Layers, linkLayer)
-
-			// configuration
-			link.Stroke = gongsvg_models.Slategray.ToString()
-			link.StrokeWidth = 3
-			link.HasEndArrow = true
-			link.EndArrowSize = 8
-
-			link.Type = gongsvg_models.LINK_TYPE_FLOATING_ORTHOGONAL
-			link.StartOrientation = gongsvg_models.ORIENTATION_HORIZONTAL
-			link.StartRatio = 0.5
-			link.EndOrientation = gongsvg_models.ORIENTATION_HORIZONTAL
-			link.EndRatio = 0.5
-			link.CornerOffsetRatio = 1.4
-			link.CornerRadius = 3
-
-			link.Start = startRect
-			link.End = endRect
-
-			// add text to the arrow
-			textAtArrowEnd := new(gongsvg_models.LinkAnchoredText).Stage(stage)
-			link.TextAtArrowEnd = append(link.TextAtArrowEnd, textAtArrowEnd)
-			link.HasEndArrow = true
-
-			textAtArrowEnd.Name = "A"
-			textAtArrowEnd.Content = textAtArrowEnd.Name
-			textAtArrowEnd.X_Offset = -50
-			textAtArrowEnd.Y_Offset = 16
-			textAtArrowEnd.Stroke = gongsvg_models.Black.ToString()
-			textAtArrowEnd.StrokeWidth = 1
-			textAtArrowEnd.Color = gongsvg_models.Black.ToString()
-			textAtArrowEnd.FillOpacity = 100
-			textAtArrowEnd.FontWeight = "normal"
-		}
-
-		for i := 0; i < *benchmark; i++ {
-
-			map_Name_Rect := (*gongsvg_models.GetGongstructInstancesMap[gongsvg_models.Rect](stage))
-			startRect := map_Name_Rect[fmt.Sprintf("Start %d", i)]
-			endRect := map_Name_Rect[fmt.Sprintf("End %d", rand.Intn(*benchmark))]
-
-			link := new(gongsvg_models.Link).Stage(stage)
-			link.Name = startRect.Name + " - to - " + endRect.Name
-
-			linkLayer := new(gongsvg_models.Layer).Stage(stage)
-
-			linkLayer.Links = append(linkLayer.Links, link)
-			svg.Layers = append(svg.Layers, linkLayer)
-
-			// configuration
-			link.Stroke = gongsvg_models.Slategray.ToString()
-			link.StrokeWidth = 3
-			link.HasEndArrow = true
-			link.EndArrowSize = 8
-
-			link.Type = gongsvg_models.LINK_TYPE_FLOATING_ORTHOGONAL
-			link.StartOrientation = gongsvg_models.ORIENTATION_HORIZONTAL
-			link.StartRatio = 0.6
-			link.EndOrientation = gongsvg_models.ORIENTATION_HORIZONTAL
-			link.EndRatio = 0.6
-			link.CornerOffsetRatio = 1.4 + float64(i)/10.0
-			link.CornerRadius = 3
-
-			link.Start = startRect
-			link.End = endRect
-
-			// add text to the arrow
-			textAtArrowEnd := new(gongsvg_models.LinkAnchoredText).Stage(stage)
-			link.TextAtArrowEnd = append(link.TextAtArrowEnd, textAtArrowEnd)
-			link.HasEndArrow = true
-
-			textAtArrowEnd.Name = "Rand"
-			textAtArrowEnd.Content = textAtArrowEnd.Name
-			textAtArrowEnd.X_Offset = -50
-			textAtArrowEnd.Y_Offset = 16
-			textAtArrowEnd.Stroke = gongsvg_models.Black.ToString()
-			textAtArrowEnd.StrokeWidth = 1
-			textAtArrowEnd.Color = gongsvg_models.Black.ToString()
-			textAtArrowEnd.FillOpacity = 100
-			textAtArrowEnd.FontWeight = "normal"
-		}
-
-		stage.Commit()
+		stage.OnInitCommitCallback = hook
 	}
 
 	gongdoc_load.Load(
@@ -310,13 +107,9 @@ func main() {
 		*embeddedDiagrams,
 		&stage.Map_GongStructName_InstancesNb)
 
-	// connects the rects to their behaviors
-	gongsvg_models.DoNotKnowWhatThisFunctionIsFor(stage)
-
-	stage.OnAfterRectUpdateCallback = new(gongsvg_models.RectOrchestrator)
-	stage.OnAfterLineUpdateCallback = new(gongsvg_models.LineOrchestrator)
-	stage.OnAfterSVGUpdateCallback = new(gongsvg_models.SVGOrchestrator)
-
-	log.Printf("Server ready serve on localhost:8080")
-	r.Run()
+	log.Printf("Server ready serve on localhost:" + strconv.Itoa(*port))
+	err := r.Run(":" + strconv.Itoa(*port))
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }

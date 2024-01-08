99efd8a20b1bafe90502b3c869277cf9e3179fe7
peugeot <thomaspeugeot@yahoo.fr>
2024-01-08 01:14:01 +0100

    refactor 1

--- ng/projects/gongsvgspecific/src/lib/gongsvg-diagramming/gongsvg-diagramming.ts
+++ ng/projects/gongsvgspecific/src/lib/gongsvg-diagramming/gongsvg-diagramming.ts
@@ -23,6 +23,7 @@ import { selectRectsByArea } from './select-rects-by-area';
 import { LinkConf, computeLinkFromMouseEvent } from '../compute.link.from.mouse.event';
 import { updateLinkFromCursor } from '../update.link.from.cursor';
 import { TextWidthCalculatorComponent } from '../text-width-calculator/text-width-calculator.component';
+import { auto_X_offset } from './auto-x-offset';
 
 @Component({
   selector: 'lib-gongsvg-diagramming',
@@ -920,75 +921,8 @@ export class GongsvgDiagrammingComponent implements OnInit, OnDestroy, AfterView
     text: gongsvg.LinkAnchoredTextDB,
     line: string,
     draggedSegmentPositionOnArrow: string): number {
-    // console.log(getFunctionName(), "text", text.Content)
 
-    let offset = 0
-    let offsetSign = 1
-
-    if (!text.AutomaticLayout) {
-      return offset
-    }
-
-    let orientation: string
-    if (draggedSegmentPositionOnArrow == gongsvg.PositionOnArrowType.POSITION_ON_ARROW_END) {
-      orientation = link.EndOrientation
-    } else {
-      orientation = link.StartOrientation
-    }
-
-    if (orientation == gongsvg.OrientationType.ORIENTATION_VERTICAL) {
-      if (text.LinkAnchorType == gongsvg.LinkAnchorType.LINK_RIGHT_OR_BOTTOM) {
-        offset += 16
-      }
-
-      if (text.LinkAnchorType == gongsvg.LinkAnchorType.LINK_LEFT_OR_TOP) {
-        let _width = this.map_text_textWidth.get(line)
-        if (_width != undefined) {
-          offset -= this.oneEm
-          offset -= _width
-          // console.log("cache hit")
-        } else {
-          if (this.textWidthCalculator != undefined) {
-            const width = this.textWidthCalculator.measureTextWidth(line);
-            // console.log(`Width of the text, "` + line + `" : ${width}px`);
-            // const height = this.textWidthCalculator.measureTextHeight(line);
-            // console.log(`Width of the text, "` + line + `" : ${height}px`);
-            offset -= this.oneEm
-            offset -= width
-            this.map_text_textWidth.set(line, width)
-          }
-        }
-      }
-    } else { // ORIENTATION_HORIZONTAL
-
-      let onTheRight = segment.EndPoint.X > segment.StartPoint.X
-      if (draggedSegmentPositionOnArrow == gongsvg.PositionOnArrowType.POSITION_ON_ARROW_END) {
-        onTheRight = !onTheRight
-      }
-
-      if (onTheRight) {
-        offset += 16
-      }
-      else {
-        let _width = this.map_text_textWidth.get(line)
-        if (_width != undefined) {
-          offset -= this.oneEm
-          offset -= _width
-          // console.log("cache hit")
-        } else {
-          if (this.textWidthCalculator != undefined) {
-            const width = this.textWidthCalculator.measureTextWidth(line);
-            // console.log(`Width of the text, "` + line + `" : ${width}px`);
-            // const height = this.textWidthCalculator.measureTextHeight(line);
-            // console.log(`Width of the text, "` + line + `" : ${height}px`);
-            offset -= this.oneEm
-            offset -= width
-            this.map_text_textWidth.set(line, width)
-          }
-        }
-      }
-    }
-
-    return offset * offsetSign
+    return auto_X_offset(link, segment, text, line, draggedSegmentPositionOnArrow,
+      this.map_text_textWidth, this.oneEm, this.textWidthCalculator!)
   }
 }
